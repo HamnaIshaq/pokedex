0f9a6dfb2848c8f7060d0351fbab57e1eacc0221
Hamna Ishaq <hamnaishaq1@gmail.com>
2022-12-02 00:20:42 +0500

    Update circle styling; width, height, color and margin

--- src/components/style-components/Circle.js
+++ src/components/style-components/Circle.js
@@ -1,25 +1,28 @@
 import styled from 'styled-components';
 
-const Circle = styled.div`
-  width: 20px;
-  height: 20px;
-  background-color: green;
+const GreenCircle = styled.div`
+  width: 30px;
+  height: 30px;
+  background-color: #2AC827;
   border-radius: 50%;
   border: 1px solid #000;
 `;
 
-const YellowCircle = styled(Circle)`
-  background-color: yellow;
+const YellowCircle = styled(GreenCircle)`
+  background-color: #D6F12C;
+  margin-left: 1.25em;
 `;
 
-const RedCircle = styled(Circle)`
-  background-color: red;
+const RedCircle = styled(GreenCircle)`
+  background-color: #FF0000;
+  margin-left: 1.25em;
 `;
 
-const BlueLargeCircle = styled(Circle)`
-  background-color: blue;
-  width: 40px;
-  height: 40px;
+const BlueLargeCircle = styled(GreenCircle)`
+  background-color: #3692E8;
+  width: 80px;
+  height: 80px;
+  margin-right: 1.25em;
 `;
 
 const YellowLargeCircle = styled(YellowCircle)`
@@ -27,4 +30,4 @@ const YellowLargeCircle = styled(YellowCircle)`
   height: 20px;
 `;
 
-export {Circle, YellowCircle, RedCircle, BlueLargeCircle, YellowLargeCircle}
+export {GreenCircle, YellowCircle, RedCircle, BlueLargeCircle, YellowLargeCircle}
