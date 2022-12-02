11d5d201348b4918400484cc1e625ec09815032d
Hamna Ishaq <hamnaishaq1@gmail.com>
2022-12-02 22:54:46 +0500

    Add search input element

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import Button from '../components/style-components/Button';
 import {GreenCircle, YellowCircle, RedCircle, BlueLargeCircle, YellowLargeCircle} from '../components/style-components/Circle';
-import {Cylinder} from '../components/style-components/Cylinder';
+import {Cylinder, SearchInput} from '../components/style-components/Cylinder';
 import './App.css';
 
 class App extends Component {
   render() {
     return(
-      <div class="pokedex-container">
+      <div className="pokedex-container">
         <div className='pokedex-top-circles-container'>
           <BlueLargeCircle/>
           <div className='pokedex-top-circles'>
@@ -18,6 +18,7 @@ class App extends Component {
         </div> 
         <YellowLargeCircle/>
         <Cylinder/>
+        <SearchInput/>
         <Button>Button</Button>
       </div>
     );
