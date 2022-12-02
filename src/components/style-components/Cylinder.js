72bdec82b187e35083b314093644018bf92fd5aa
Hamna Ishaq <hamnaishaq1@gmail.com>
2022-12-02 23:48:42 +0500

    Add container element styling

--- src/components/style-components/Cylinder.js
+++ src/components/style-components/Cylinder.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 const Cylinder = styled.div`
   width: 90px;
   height: 20px;
-  background-color: green;
+  background-color: #84BD93;
   border-radius: 50px;
   border: 1px solid #000;
 `;
@@ -20,4 +20,27 @@ const SearchInput = styled.input`
   font-family: inherit;
 `;
 
-export {Cylinder, SearchInput}
+const ImageContainer = styled.div`
+  width: 100%;
+  height: 150px;
+  background-color: #000;
+  border: none;
+  color: #fff;
+  font-size: .75rem;
+  font-family: inherit;
+  display: flex;
+  align-items: center;
+`;
+
+const ContentContainer = styled(ImageContainer)`
+  min-height: 100px;
+  height: fit-content;
+  background-color: lightgray;
+  border: 1px solid #000;
+  color: #000;
+  padding-left: 1em;
+  padding-right: 1em;
+  line-height: 1.2;
+`;
+
+export {Cylinder, SearchInput, ImageContainer, ContentContainer}
